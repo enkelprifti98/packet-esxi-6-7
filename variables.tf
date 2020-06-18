aef5382834df54aa4f6615f8d34e7e74985b9b21
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-06-18 18:10:25 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,3 +32,9 @@ variable "esxi_update_filename" {
   description = "The specific update version that your servers will be updated to. Note that the Packet portal and API will still show ESXi 6.5 as the OS but this script adds a tag with the update filename specified below. You can check all ESXi update versions/filenames here: https://esxi-patches.v-front.de/ESXi-6.7.0.html"
   default = "ESXi-6.7.0-20191104001-standard"
 }
+
+variable "public_ipv4_subnet_size" {
+  description = "Public IPv4 management subnet size, size should be set as the total amount of IPs in the subnet. Ex: For /28 subnet, amount = 16. /29 is default for ESXi on Packet."
+  default = 8
+}
+
